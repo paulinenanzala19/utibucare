5956875f2e4301952647517a94d58072d1e0514e
paulinenanzala <paulinenanzala19@gmail.com>
2024-04-02 17:51:29 +0300

    deploying to railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn utibuhealth_care.wsgi --log-file -
--- utibuhealth_care/settings.py
+++ utibuhealth_care/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ldab7$xj@13g(2^o=u4)x1ho^9yn#fx_!$4%t18s+pn2s$yl0a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
